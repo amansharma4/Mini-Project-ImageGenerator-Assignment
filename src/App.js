bd80cd19870e51eb2fd7bf655ccb2ad8ecc51f46
Aman Sharma <amans7990@gmail.com>
2020-12-04 12:52:18 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import logo from "./logo.svg";
 import "./css/App.css";
-import React from "react";
-
+import React, { useState } from "react";
+// import ImageDisplay from "./component/ImageDisplay";
 function App() {
+  const [image, setImage] = useState("");
+  const getImage = () => {
+    fetch("https://picsum.photos/200/300")
+      // .then((result) => result.json())
+      .then((data) => {
+        const { url } = data;
+
+        // console.log(url);
+        setImage(url);
+      });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      {/* <ImageDisplay /> */}
+      <br />
+      <h1>{image}</h1>
+      <button onClick={getImage}>Get New Image</button>
     </div>
   );
 }
---
+++ src/component/ImageDisplay.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ImageDisplay = () => {
+  return (
+    <div>
+      <img src="" alt="img"></img>
+    </div>
+  );
+};
+export default ImageDisplay;
