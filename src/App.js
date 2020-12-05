3f284ad457bc45d89aa6f23da3f5225e79905d4c
Aman Sharma <amans7990@gmail.com>
2020-12-05 13:08:56 +0530

    Up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import "./css/App.css";
 import React, { useState } from "react";
-// import ImageDisplay from "./component/ImageDisplay";
+import ImageDisplay from "./component/ImageDisplay";
 function App() {
   const [image, setImage] = useState("");
   const getImage = () => {
-    fetch("https://picsum.photos/200/300")
-      // .then((result) => result.json())
-      .then((data) => {
-        const { url } = data;
-
-        // console.log(url);
-        setImage(url);
-      });
+    fetch("https://picsum.photos/200/300").then((data) => {
+      const { url } = data;
+      url.length > 0 ? setImage(url) : setImage("Image not found");
+      // console.log(data);
+    });
   };
   return (
     <div className="App">
-      {/* <ImageDisplay /> */}
-      <br />
-      <h1>{image}</h1>
+      <ImageDisplay image={image} />
+
       <button onClick={getImage}>Get New Image</button>
     </div>
   );
--- src/component/ImageDisplay.js
+++ src/component/ImageDisplay.js
@@ -1,10 +1,13 @@
 import React from "react";
-
-const ImageDisplay = () => {
-  return (
-    <div>
-      <img src="" alt="img"></img>
-    </div>
-  );
+const ImageDisplay = (props) => {
+  if (props.image) {
+    return (
+      <div>
+        <img src={props.image} alt="disp" />
+      </div>
+    );
+  } else {
+    return <div>Loading.........</div>;
+  }
 };
 export default ImageDisplay;
