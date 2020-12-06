3b318d77cdf5db4ffa0486e504e5f8cbbc8e185d
Aman Sharma <amans7990@gmail.com>
2020-12-06 12:40:28 +0530

    up

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import "./css/App.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ImageDisplay from "./component/ImageDisplay";
 function App() {
   const [image, setImage] = useState("");
+  // const [loading, setLoading] = useState(false);
   const getImage = () => {
-    fetch("https://picsum.photos/200/300").then((data) => {
-      const { url } = data;
-      setImage(url);
-      // console.log(data);
-    });
+    try {
+      fetch("https://picsum.photos/200/300").then((data) => {
+        const { url } = data;
+        setImage(url);
+        // console.log(data);
+      });
+      // setLoading(true);
+    } catch (e) {
+      console.log(e);
+    }
   };
+  useEffect(() => {
+    getImage();
+  }, []);
   return (
     <div className="App">
       <ImageDisplay image={image} />
