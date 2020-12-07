f8832d15dcc7eded9934ea7c3a5ec2a44f05a7b7
Aman Sharma <amans7990@gmail.com>
2020-12-07 17:45:44 +0530

    Add

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ function App() {
   // const [loading, setLoading] = useState(false);
   const getImage = () => {
     try {
+      setImage("");
       fetch("https://picsum.photos/200/300").then((data) => {
         const { url } = data;
         setImage(url);
         // console.log(data);
       });
-      // setLoading(true);
     } catch (e) {
       console.log(e);
     }
